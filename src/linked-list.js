d4884b0b764539d0e3237d25c59a7cf8233a0d1d
Krishna Regmi <kisna72@gmail.com>
2019-05-15 22:47:17 -0600

    comments

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,4 +1,4 @@
-//A Blockchain is a linked list, so to replicate a blockchain, we will implement the most basic Linked List. 
+//A Blockchain is a linked list, so to replicate a blockchain, we will implement the most basic Linked List - with just the pieces we need.
 function Node(element){
   this.element = element;
   this.prev = null;
